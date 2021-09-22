ff1c91c4347f22b526cd6647bc21081de7513dd7
xizh <995194437@qq.com>
2021-09-22 16:09:05 +0800

    init 初步完成

--- src/components/app.js
+++ src/components/app.js
@@ -2,32 +2,44 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch, Redirect, Link } from "react-router-dom"
 import Home from '@/views/home/index'
 import Profile from '@/views/profile/index'
-class App extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            text: 'app container'
-        }
-    }
-    render(){
-        return (
-            <div className='app-container'>
-                {this.state.text}
-                <Router>
-                    <Link to="/index">Home</Link>
-                    <Link to="/profile">Profile</Link>
+// class App extends React.Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             text: 'app container'
+//         }
+//     }
+//     render(){
+//         return (
+//             <div className='app-container'>
+//                 <p>{this.state.text}</p>
+//                 <Link to="/index">Home</Link>
+//                 <Link to="/profile">Profile</Link>
 
-                    <Switch>
-                        <Route exact path={'/index'} component={Home}></Route>
-                        <Route exact path={'/profile'} component={Profile}></Route>
-                        <Redirect from='/' to='/index'></Redirect>
+//                 <Switch>
+//                     <Route exact path={'/index'} component={Home}></Route>
+//                     <Route exact path={'/profile'} component={Profile}></Route>
+//                     <Redirect from='/' to='/index'></Redirect>
 
-                    </Switch>
-                </Router>
-            </div>
-        )
-    }
+//                 </Switch>
+//             </div>
+//         )
+//     }
+// }
+
+export default function App (){
+    return (
+        <div className='app-container'>
+            <p>首页</p>
+            <Link to="/index">Home</Link>
+            <Link to="/profile">Profile</Link>
+
+            <Switch>
+                <Route exact path={'/index'} component={Home}></Route>
+                <Route exact path={'/profile'} component={Profile}></Route>
+                <Redirect from='/' to='/index'></Redirect>
+
+            </Switch>
+        </div>
+    )
 }
-
-export default App
-
