868d71be5d253c2fc691e268d4cca3975518b496
xizh <995194437@qq.com>
2021-09-22 18:03:56 +0800

    安装file-loader

--- src/views/profile/index.js
+++ src/views/profile/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
-export default function Profile(){
-    return (
-        <div className="profile-wrapper">
-            <p>Profile页面</p>
-        </div>
-    )
+import defaultAvatar from '@/assets/images/yyx.jpeg'
+class Profile extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            defaultAvatar,
+        }
+    }
+    render(){
+        return (
+            <div className="profile-wrapper">
+                <p>Profile页面</p>
+                <img src={this.state.defaultAvatar} alt="头像"></img>
+            </div>
+        )
+    }
 }
+
+export default Profile
