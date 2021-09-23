31c29842ee22f4c208b90c6ae69bec1a10d6f541
xizh <995194437@qq.com>
2021-09-23 10:31:03 +0800

    新增工具类

--- src/components/app.js
+++ src/components/app.js
@@ -2,30 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch, Redirect, Link } from "react-router-dom"
 import Home from '@/views/home/index'
 import Profile from '@/views/profile/index'
-// class App extends React.Component {
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             text: 'app container'
-//         }
-//     }
-//     render(){
-//         return (
-//             <div className='app-container'>
-//                 <p>{this.state.text}</p>
-//                 <Link to="/index">Home</Link>
-//                 <Link to="/profile">Profile</Link>
-
-//                 <Switch>
-//                     <Route exact path={'/index'} component={Home}></Route>
-//                     <Route exact path={'/profile'} component={Profile}></Route>
-//                     <Redirect from='/' to='/index'></Redirect>
-
-//                 </Switch>
-//             </div>
-//         )
-//     }
-// }
+import '@/styles/common.scss'
 
 export default function App (){
     return (
